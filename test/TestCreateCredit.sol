eeceb8f5ae93eb2582779e29640e1a0a70ee00e0
Tim <tim@Tims-MacBook-Pro.local>
2019-11-14 03:01:13 +0700

    testt: create test cases for creating asset type

--- test/TestCreateCredit.sol
+++ test/TestCreateCredit.sol
@@ -2,31 +2,33 @@ pragma solidity >=0.4.25 <0.6.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/ERC1155MixedFungibleMintable.sol";
+import "../contracts/ERC1155e.sol";
 
 contract TestCreateCredit {
 
-    ERC1155MixedFungibleMintable private credit;
+    ERC1155E private credit;
+
+    event addressLogger(address indexed sender);
 
     function beforeEach() external {
-      credit = new ERC1155MixedFungibleMintable();
+      credit = new ERC1155E();
     }
 
     function testCreateFungibleTypes() external {
         string memory expectedURI = "foo";
         bool isNF = false;
         uint256 expectedID = 1 << 128;
-        credit.create(expectedURI, isNF);
+        uint256 actualID = credit.create(expectedURI, isNF);
         Assert.isTrue(credit.isFungible(expectedID), "credit type should be non-fundgible");
-        Assert.equal(credit.creators(expectedID), msg.sender, "message sender should be the one who created the type");
+        Assert.equal(actualID, expectedID, "ID from create function should be equal to expected ID");
     }
 
-    // function testCreateNFTypes() external {
-    //     string memory expectedURI = "foo";
-    //     bool isNF = false;
-    //     uint256 expectedID = 1 << 128 | ;
-    //     credit.create(expectedURI, isNF);
-    //     Assert.isTrue(credit.isNonFungible(expectedID), "credit type should be non-fundgible");
-    //     Assert.equal(credit.creators(expectedID), msg.sender, "message sender should be the one who created the type");
-    // }
+    function testCreateNFTypes() external {
+        string memory expectedURI = "foo";
+        bool isNF = true;
+        uint256 expectedID = 1 << 128 | 1 << 255 ;
+        uint256 actualID = credit.create(expectedURI, isNF);
+        Assert.isTrue(credit.isNonFungible(expectedID), "credit type should be non-fundgible");
+        Assert.equal(actualID, expectedID, "ID from create function should be equal to expected ID");
+    }
 }

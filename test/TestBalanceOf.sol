52b770e1747ada136d3fae2c5789a28941fcd50b
Pakkapol Rattanapongsen <jui@r0ix.com>
2019-11-14 14:59:14 +0700

    test: add error case for the balance of batch

--- test/TestBalanceOf.sol
+++ test/TestBalanceOf.sol
@@ -1,11 +1,21 @@
 pragma solidity ^0.5.0;
 
 import "truffle/Assert.sol";
+import "./utils/ThrowProxy.sol";
 import "../contracts/ERC1155e.sol";
+
+contract ERC1155EWrapper is ERC1155E {
+    constructor() public ERC1155E(){}
+
+    function callBalanceOfBatch(address[] calldata _owners, uint256[] calldata _ids, uint256[] calldata _expecteds) external {
+        Assert.equal(this.balanceOfBatch(_owners, _ids), _expecteds, "balances are invalid");
+    }
+}
 
 contract TestBalanceOf {
 
-    ERC1155E private credit;
+    ERC1155EWrapper private credit;
+    ThrowProxy private throwProxy;
     uint256 private fungibleCreditID;
     uint256 private nonFungibleCreditID;
     uint256 private expectedBalance = 100;
@@ -13,7 +23,8 @@ contract TestBalanceOf {
     address private barAccount = address(2);
 
     function beforeEach() external {
-        credit = new ERC1155E();
+        credit = new ERC1155EWrapper();
+        throwProxy = new ThrowProxy(address(credit));
         fungibleCreditID = credit.create("", false);
         uint256 nonFungibleCreditType = credit.create("", true);
 
@@ -54,6 +65,31 @@ contract TestBalanceOf {
         expecteds[2] = 0;
         expecteds[3] = 0;
 
-        Assert.equal(credit.balanceOfBatch(owners, ids), expecteds, "balances are invalid");
+        ERC1155EWrapper(address(throwProxy)).callBalanceOfBatch(owners, ids, expecteds);
+        (bool success, ) = throwProxy.execute.gas(200000)();
+        Assert.isTrue(success, "should not throw error");
+    }
+
+    function testErrorBalanceOfBatch() external {
+        address[] memory owners = new address[](4);
+        owners[0] = fooAccount;
+        owners[1] = fooAccount;
+        owners[2] = barAccount;
+        owners[3] = barAccount;
+
+        uint256[] memory ids = new uint256[](3);
+        ids[0] = fungibleCreditID;
+        ids[1] = nonFungibleCreditID;
+        ids[2] = fungibleCreditID;
+
+        uint256[] memory expecteds = new uint256[](4);
+        expecteds[0] = expectedBalance;
+        expecteds[1] = 1;
+        expecteds[2] = 0;
+        expecteds[3] = 0;
+
+        ERC1155EWrapper(address(throwProxy)).callBalanceOfBatch(owners, ids, expecteds);
+        (bool success, ) = throwProxy.execute.gas(200000)();
+        Assert.isFalse(success, "should throw error");
     }
 }

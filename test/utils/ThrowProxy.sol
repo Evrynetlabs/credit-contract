f84110ad328697212a57c218c21721816c24515f
Pakkapol Rattanapongsen <jui@r0ix.com>
2019-11-15 17:46:33 +0700

    refactor: separate new throw proxy to another type call PayableThrowProxy

--- test/utils/ThrowProxy.sol
+++ test/utils/ThrowProxy.sol
@@ -2,11 +2,7 @@ pragma solidity ^0.5.0;
 
 import "../../contracts/IERC1155TokenReceiver.sol";
 
-contract ThrowProxy is IERC1155TokenReceiver{
-
-    event Received(address _operator, address _from, uint256 _id, uint256 _value, bytes _data);
-    event BatchReceived(address _operator, address _from, uint256[] _ids, uint256[] _values, bytes _data);
-
+contract ThrowProxy {
     address public target;
     bytes data;
 
@@ -25,17 +21,5 @@ contract ThrowProxy is IERC1155TokenReceiver{
             return target.call.value(address(this).balance)(data);
         }
         return target.call(data);
-    }
-
-    /////////////////////////////////////////// IERC1155TokenReceiver //////////////////////////////////////////////
-
-    function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4){
-        emit Received(_operator, _from, _id, _value, _data);
-        return 0xf23a6e61;
-    }
-
-    function onERC1155BatchReceived(address _operator, address _from, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4){
-        emit BatchReceived(_operator, _from, _ids, _values, _data);
-        return 0xbc197c81;
     }
 }

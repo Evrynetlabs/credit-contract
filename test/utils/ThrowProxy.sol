1e498d0531b536ed5c9fe35e240e8be75379c6c9
Pakkapol Rattanapongsen <jui@r0ix.com>
2019-11-15 17:24:02 +0700

    feat: throw proxy now can receive ERC1155E credit

--- test/utils/ThrowProxy.sol
+++ test/utils/ThrowProxy.sol
@@ -1,6 +1,12 @@
 pragma solidity ^0.5.0;
 
-contract ThrowProxy {
+import "../../contracts/IERC1155TokenReceiver.sol";
+
+contract ThrowProxy is IERC1155TokenReceiver{
+
+    event Received(address _operator, address _from, uint256 _id, uint256 _value, bytes _data);
+    event BatchReceived(address _operator, address _from, uint256[] _ids, uint256[] _values, bytes _data);
+
     address public target;
     bytes data;
 
@@ -19,5 +25,17 @@ contract ThrowProxy {
             return target.call.value(address(this).balance)(data);
         }
         return target.call(data);
+    }
+
+    /////////////////////////////////////////// IERC1155TokenReceiver //////////////////////////////////////////////
+
+    function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4){
+        emit Received(_operator, _from, _id, _value, _data);
+        return 0xf23a6e61;
+    }
+
+    function onERC1155BatchReceived(address _operator, address _from, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4){
+        emit BatchReceived(_operator, _from, _ids, _values, _data);
+        return 0xbc197c81;
     }
 }
